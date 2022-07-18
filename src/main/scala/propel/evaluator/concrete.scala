0c0c397e2d92ec9cc8e5bd24b6f0bace74cbd750
Pascal Weisenburger <pascal.weisenburger@web.de>
2022-07-19 00:30:59 +0200

    add concrete call-by-value evaluator

---
+++ src/main/scala/propel/evaluator/concrete.scala
@@ -0,0 +1,48 @@
+package propel
+package evaluator
+
+import ast.*
+import util.*
+
+object Concrete:
+  def eval(term: Term): Term = term match
+    case Abs(_, _, _) =>
+      term
+    case App(properties, expr, arg) =>
+      let(eval(arg)) { arg =>
+        let(arg.withInfo(Syntactic.Term)) { (arg, argInfo) =>
+          eval(expr) match
+            case Abs(_, ident, expr) if argInfo.closed && argInfo.value =>
+              eval(subst(expr, Map(ident -> arg)))
+            case expr =>
+              App(term)(properties, expr, arg)
+        }
+      }
+    case Data(ctor, args) =>
+      Data(term)(ctor, args map eval)
+    case Var(_)=>
+      term
+    case Cases(scrutinee, cases) =>
+      let(eval(scrutinee)) { scrutinee =>
+        def process(processingCases: List[(Pattern, Term)]): Term = processingCases match
+          case Nil =>
+            Cases(term)(scrutinee, cases)
+          case (pattern, expr) :: tail =>
+            Unification.unify(pattern, scrutinee) match
+              case Unification.Full(substs) =>
+                val substsInfos = substs.view mapValues { _.withInfo(Syntactic.Term) }
+                if substsInfos.values forall { (_, info) => info.closed && info.value } then
+                  eval(subst(expr, (substsInfos mapValues { (expr, _) => expr }).toMap))
+                else
+                  Cases(term)(scrutinee, List(pattern -> expr))
+
+              case Unification.Irrefutable(_, _) =>
+                Cases(term)(scrutinee, List(pattern -> expr))
+
+              case _ =>
+                process(tail)
+
+        process(cases)
+      }
+  end eval
+end Concrete
